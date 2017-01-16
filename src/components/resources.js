b93a69c40e21cc1a3fea62c10bd9ec8a847372ea
drabelpdx <drabelpdx@gmail.com>
2017-01-16 11:59:29 -0800

    Adds Authentication to HOC

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Secret Recipe
+      <ul>
+        <li>ingredient 1</li>
+        <li>ingredient 2</li>
+        <li>ingredient 3</li>
+      </ul>
+    </div>
+  );
+};
